b62705a6c529bf07cb86eba6bb1bf2950bc501cf
omdhede <omdhede@gmail.com>
2024-10-13 23:20:40 +0530

    Use Clippy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+#![warn(clippy::all, clippy::pedantic)]
 mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
+    let editor = Editor::default();            
     editor.run();
 }
