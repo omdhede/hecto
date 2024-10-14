5b5e8017ab2082601bd47af287cfdda8870cb3cf
omdhede <omdhede@gmail.com>
2024-10-14 11:37:44 +0530

    feat/Adding tildes

--- target/debug/deps/hecto-096a275dd9694d3e.d
+++ target/debug/deps/hecto-096a275dd9694d3e.d
@@ -1,9 +1,10 @@
-/Users/omd/Developer/text/hecto/target/debug/deps/libhecto-096a275dd9694d3e.rmeta: src/main.rs src/editor.rs Cargo.toml
+/Users/omd/Developer/text/hecto/target/debug/deps/libhecto-096a275dd9694d3e.rmeta: src/main.rs src/editor.rs src/editor/terminal.rs Cargo.toml
 
-/Users/omd/Developer/text/hecto/target/debug/deps/hecto-096a275dd9694d3e.d: src/main.rs src/editor.rs Cargo.toml
+/Users/omd/Developer/text/hecto/target/debug/deps/hecto-096a275dd9694d3e.d: src/main.rs src/editor.rs src/editor/terminal.rs Cargo.toml
 
 src/main.rs:
 src/editor.rs:
+src/editor/terminal.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=
--- target/debug/deps/hecto-166a89a194f907bb.d
+++ target/debug/deps/hecto-166a89a194f907bb.d
@@ -1,6 +1,7 @@
-/Users/omd/Developer/text/hecto/target/debug/deps/hecto-166a89a194f907bb: src/main.rs src/editor.rs
+/Users/omd/Developer/text/hecto/target/debug/deps/hecto-166a89a194f907bb: src/main.rs src/editor.rs src/editor/terminal.rs
 
-/Users/omd/Developer/text/hecto/target/debug/deps/hecto-166a89a194f907bb.d: src/main.rs src/editor.rs
+/Users/omd/Developer/text/hecto/target/debug/deps/hecto-166a89a194f907bb.d: src/main.rs src/editor.rs src/editor/terminal.rs
 
 src/main.rs:
 src/editor.rs:
+src/editor/terminal.rs:
